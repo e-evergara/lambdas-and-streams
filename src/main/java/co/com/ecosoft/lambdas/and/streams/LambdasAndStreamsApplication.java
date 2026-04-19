4525496453ac37729feb915dfe9a0d3bced50969
evergara <emersonjb@gmail.com>
2026-04-19 07:34:59 -0500

    first commit

---
+++ src/main/java/co/com/ecosoft/lambdas/and/streams/LambdasAndStreamsApplication.java
@@ -0,0 +1,13 @@
+package co.com.ecosoft.lambdas.and.streams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdasAndStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdasAndStreamsApplication.class, args);
+	}
+
+}
